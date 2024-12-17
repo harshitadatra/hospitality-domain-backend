d014f635ab1a6d0ca2d8a3a53e96b77a4b44ef09
harshitadatra <harshitkiran22@gmail.com>
2024-12-17 23:52:49 +0530

    added user schema

---
+++ db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+// mongoose.connect('')
+
+const userSchema =  new mongoose.Schema({
+    userId :String,
+    name:String,
+    address: String,
+    email:String,
+    phoneNo :Number,
+    password:String,
+    userBooking:Array,
+})
+
+const userModel = mongoose.model("user",userSchema);
+module.exports = userModel
