44a64a03864558f5ad848e8ad848decbf82006b8
harshitadatra <harshitkiran22@gmail.com>
2024-12-21 02:11:00 +0530

    refactor: removed unwanted code

--- db.js
+++ db.js
@@ -3,8 +3,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const DB_URL = process.env.DB_URL;
-// console.log("DB_URL", DB_URL);
-//  mongoose.connect('mongodb+srv://harshit22:Hk7YJZ8C1D18PQgv@cluster0.erwxsd6.mongodb.net/hospitality-domain')
  mongoose.connect(DB_URL)
 
 
