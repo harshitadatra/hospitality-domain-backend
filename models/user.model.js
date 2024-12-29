cbb0f2f6fd4c61d7d82fb9f35d32224e4803201b
harshitadatra <harshitkiran22@gmail.com>
2024-12-30 02:17:04 +0530

    feat: added hotel and booking Schema

--- models/user.model.js
+++ models/user.model.js
@@ -1,13 +1,32 @@
 const mongoose = require('mongoose')
-const userSchema =  new mongoose.Schema({
-    userId :String,
-    name:String,
+const userSchema = new mongoose.Schema({
+    userId: String,
+    name: String,
     address: String,
-    email:String,
-    phoneNo :Number,
-    password:String,
-    userBooking:Array,
+    email: String,
+    phoneNo: Number,
+    password: String,
+    userBooking: Array,
+})
+const hotelSchema = new mongoose.Schema({
+    HotelName: String,
+    Description: String,
+    Amenities: String,
+    PhoneNo: Number,
+    Address: String,
+    Reviews: Array
 })
 
-const userModel = mongoose.model("user",userSchema);
-module.exports = userModel
+const bookingSchema = new mongoose.Schema({
+    BookingId: String,
+    StartDate: Date,
+    EndDate: Date,
+    NoOfPersons: Number,
+    NoOfRooms: Number,
+    TypeOfRoom: String
+
+})
+const userModel = mongoose.model("user", userSchema);
+const hotelModel = mongoose.model("hotel",hotelSchema);
+const bookingModel = mongoose.model("booking",bookingSchema)
+module.exports = {userModel,hotelModel,bookingModel}
